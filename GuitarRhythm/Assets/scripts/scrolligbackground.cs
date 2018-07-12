73358177e4e84dc36f1c56ee9ec636ca857b2209
Mohamud <detectivemomo1@gmail.com>
2018-07-11 22:18:29 -0400

    no changes

--- GuitarRhythm/Assets/scripts/scrolligbackground.cs
+++ GuitarRhythm/Assets/scripts/scrolligbackground.cs
@@ -4,20 +4,24 @@ using UnityEngine;
 
 public class scrolligbackground : MonoBehaviour {
 
+	private GameObject[] backgroundkids;
+	private Transform [] layers;
 
+	private int Index;
+	public float speed;
 
 	void Start () {
+		layers = new Transform[transform.childCount];
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		Index = 0;
+		moveobject ();
 	}
-	void moveobjectright(){
-		
+	void moveobject(){
+		layers [Index].Translate((Vector3.right * speed) * Time.deltaTime);
 	}
-	void moveobjectleft(){
 
-	}
 }
