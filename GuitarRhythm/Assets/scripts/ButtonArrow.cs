2308da439984c23a06f2b7629417581a1067d3e7
Mohamud <detectivemomo1@gmail.com>
2018-08-02 15:12:31 -0400

    add arrow and code to the button

--- GuitarRhythm/Assets/scripts/ButtonArrow.cs
+++ GuitarRhythm/Assets/scripts/ButtonArrow.cs
@@ -1,12 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ButtonArrow : MonoBehaviour {
 
 	private ExtendButtons m_Button;
 
 	[SerializeField] private GameObject m_Arrow;
+
+	[SerializeField] private Text m_ButtonText;
+	[SerializeField] private List <float> SelectTextRGB  = new List<float>();
+	[SerializeField] private List <float> DeselectTextRGB  = new List<float>();
 
 	// Use this for initialization
 
@@ -16,6 +21,9 @@ public class ButtonArrow : MonoBehaviour {
 		m_Button.OnButtonSelect += ShowArrow;
 		m_Button.OnButtonDeSelect += HideArrow;
 
+		m_Button.OnButtonSelect += SelectText;
+		m_Button.OnButtonDeSelect += DeselectText;
+
 		HideArrow ();
 	}
 
@@ -24,6 +32,9 @@ public class ButtonArrow : MonoBehaviour {
 		{
 			m_Button.OnButtonSelect -= ShowArrow;
 			m_Button.OnButtonDeSelect -= HideArrow;
+
+			m_Button.OnButtonSelect -= SelectText;
+			m_Button.OnButtonDeSelect -= DeselectText;
 		}
 	}
 
@@ -42,6 +53,27 @@ public class ButtonArrow : MonoBehaviour {
 		if (m_Arrow != null)
 		{
 			m_Arrow.SetActive (false);
+		}
+
+
+	}
+
+	private void SelectText () {
+
+		if (m_ButtonText != null &  SelectTextRGB != null  &  SelectTextRGB.Count > 3)
+		{
+			m_ButtonText.color =  new Color( SelectTextRGB[0], SelectTextRGB[1], SelectTextRGB[2]);
+		}
+
+
+	}
+
+
+	private void DeselectText () {
+
+		if (m_ButtonText != null &  DeselectTextRGB != null  &  DeselectTextRGB.Count > 3)
+		{
+			m_ButtonText.color =  new Color( DeselectTextRGB[0], DeselectTextRGB[1],  DeselectTextRGB[2]);
 		}
 
 
