87cc91dc6c542d5598c5bd667d347ea3f40e2ebe
Mohamud <detectivemomo1@gmail.com>
2018-08-07 23:35:32 -0400

    add  class timer with carlo

---
+++ GuitarRhythm/Assets/scripts/Timer.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer  {
+	#region Variable and Properties
+	//Public 
+	public event System.Action m_OnDone;
+	public event System.Action m_OnUpdate;
+
+	//Private  
+	private float  m_CurrentTime = 0.0f;
+	private float  m_TotalTime = 0.0f;
+
+	private bool m_IsStarted = false;
+	private bool m_IsPaused = false;
+	private bool m_IsDone = false;
+
+
+	//Properties 
+	public float RemainingTime {get {return m_TotalTime - m_CurrentTime;} }
+	#endregion
+
+	#region Public Methods
+	public Timer (float time)
+	{
+		m_CurrentTime = 0.0f;
+		m_TotalTime = time;
+		m_IsPaused = false;
+		m_IsStarted = false;
+		m_IsDone = false;
+
+	}
+	
+	public void Start()
+	{
+		m_IsStarted = true;
+		m_IsPaused = false;
+		m_IsDone = false;
+		
+	}
+	public void Pause(bool isPaused)
+	{
+		m_IsPaused = isPaused;
+	}
+
+	public void Restart()
+	{
+		m_CurrentTime = 0.0f;
+		m_IsPaused = false;
+		m_IsStarted = true;
+		m_IsDone = false;
+	}
+	public void Stop()
+	{
+		m_IsStarted = false;
+
+	}
+
+	public void Update()
+	{
+		if (m_IsStarted && !m_IsDone) {
+			if (!m_IsPaused) {
+				
+				if (m_CurrentTime < m_TotalTime) {
+
+					m_CurrentTime += Time.deltaTime;
+
+					if (m_OnUpdate != null) {
+						m_OnUpdate ();
+					}
+
+				} else {
+					m_IsDone = true;
+					//if(
+					
+					//}
+				}
+			}
+		}
+
+
+	}
+
+	
+#endregion
+	}
--- GuitarRhythm/Assets/scripts/test/MainMeun.cs
+++ GuitarRhythm/Assets/scripts/test/MainMeun.cs
@@ -2,14 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems; 
 
 public class MainMeun : MonoBehaviour {
 
+
 	[SerializeField] private Selectable m_StartUIElement;
-	// Use this for initialization
+
+	//Use this for initialization
+
 	private void Start () {
 		if (m_StartUIElement != null) {
 			m_StartUIElement.Select ();
+
+		
 		}
 	}
 	
