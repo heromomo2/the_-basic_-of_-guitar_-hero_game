db9179ab856959d842db5c5f33efc70ba4a6eb96
Mohamud <detectivemomo1@gmail.com>
2018-08-09 18:30:59 -0400

    add some canvas

--- GuitarRhythm/Assets/scripts/test/MeunController.cs
+++ GuitarRhythm/Assets/scripts/test/MeunController.cs
@@ -4,19 +4,23 @@ using UnityEngine.SceneManagement;
 using UnityEngine;
 public class MeunController : MonoBehaviour {
 
-
+	#region  Variable for Canvas
+	[SerializeField] private Canvas OptionMeunGeneralCanvas;
+	[SerializeField] private Canvas MainMeunCanvas;
+	#endregion
 
 	public void OpenPlayMeun()
 	{
-		
+		MainMeunCanvas.enabled = true ; 
+		OptionMeunGeneralCanvas.enabled = false;
 
 	}
 
 	public void OpenOptionMeun()
-	{
-		
-
+	{  MainMeunCanvas.enabled = false ; 
+		OptionMeunGeneralCanvas.enabled = true;
 	}
+
 	public void LoadA( string scenename)
 	{
 		Debug.Log ("sceneName to load:" + scenename);
