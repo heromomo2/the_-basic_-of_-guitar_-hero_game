874029a3f3da8fd75df405d90cb7434897c0370f
Mohamud <detectivemomo1@gmail.com>
2019-01-29 15:21:02 -0500

    working task 007
    Start work: 12:30 pm
    -  I have create a class called Rows. Also add list of bool data type called(m_RowOfNote) and add a float data type called (m_TimeSignature).
    - Comments out of some of code in the SongChart code.
    - Still trying to figure out how the Rows class need to function with Songchart class.
    - Add System.serializable to the top of Rows class.
    End work: 3:03 pm

---
+++ GuitarRhythm/Assets/Scripts/RhythemGame/Rows.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+
+public class Rows : MonoBehaviour {
+
+	[SerializeField]
+	private List<bool> m_RowOfNote;
+	[SerializeField]
+	private float m_TimeSignature; 
+
+
+
+}
--- GuitarRhythm/Assets/Scripts/RhythemGame/SongChart.cs
+++ GuitarRhythm/Assets/Scripts/RhythemGame/SongChart.cs
@@ -5,26 +5,45 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "NewSong", menuName ="RhythmGame/Song", order = 1)]
 public class SongChart : ScriptableObject {
 
+	/*[SerializeField] 
+	private List <string> m_Row = new List<string> ();*/
+
 	[SerializeField] 
-	private List <string> m_Row = new List<string> ();
+	private List <Rows> m_Row = new List<Rows> ();
 
 	[SerializeField]
 	private float m_Time = 0;
 
-	public List<string>Rows
+	/*public List<string>Rows
+	{
+		get {return m_Row; }
+	}*/
+
+
+	/*public float Time
+	{
+		get {return m_Time;}
+	}*/
+
+	public List<> Rows
 	{
 		get {return m_Row; }
 	}
 
 	public float Time
 	{
-		get {return m_Time;}
+		get {return m_Row.;}
 	}
+
+  /*[ContextMenu ("Reverse Rows")]
+	private void Reverse()
+	{
+		m_Row.Reverse ();
+	}*/
 
 	[ContextMenu ("Reverse Rows")]
 	private void Reverse()
 	{
 		m_Row.Reverse ();
 	}
-
 }
